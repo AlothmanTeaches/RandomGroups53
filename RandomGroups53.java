0f64ee36ad2e73d49ad58b9d5faa4b5e0b9490cd
alothmanmj <alothman.maj@gmail.com>
2023-02-07 14:38:23 +0300

    show version 3

--- RandomGroups53.java
+++ RandomGroups53.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class RandomGroups53{
     public static void main(String[] args) {
         System.out.println("Works fine");
-        System.out.println("V2");
+        System.out.println("V3");
         System.out.println(readStu());
     }
 
