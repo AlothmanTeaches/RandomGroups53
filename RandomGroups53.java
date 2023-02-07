6c8bf7178d65135c83fb069c727bd5b14455475d
alothmanmj <alothman.maj@gmail.com>
2023-02-07 14:53:03 +0300

    show version 7

--- RandomGroups53.java
+++ RandomGroups53.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class RandomGroups53{
     public static void main(String[] args) {
         System.out.println("Works fine");
-        System.out.println("V3");
+        System.out.println("V7");
         System.out.println(readStu());
         randomGroups(readStu(), 5);
     }
